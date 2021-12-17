99ee5c14b4ae1d1310a4b542feb15bc7d4c18d0c
Arison13 <arisonarias@gmail.com>
2021-12-17 13:02:52 -0500

    tests built

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,61 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const server = require("./server");
+const request = require("supertest");
+const db = require("./../data/dbConfig");
+
+beforeEach(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+
+describe("[POST] /register", () => {
+  let res;
+  beforeEach(async () => {
+    res = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "parce", password: "fairies" });
+  });
+
+  it("responds with a 201 created", async () => {
+    expect(res.status).toBe(201);
+  });
+
+  it("causes a user to be added to the db", async () => {
+    const users = await db("users");
+    expect(users).toHaveLength(1);
+  });
+
+  it("responds with newly created user", async () => {
+    expect(res.body).toMatchObject({ id: 1, username: "parce" });
+  });
+});
+
+describe("[POST] /login", () => {
+  let res
+  beforeEach(async() => {
+    await request(server)
+      .post('/api/auth/register')
+      .send({ username: "marego", password: "12345" })
+
+    res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: "marego", password: "12345" })
+  })
+  
+  it("responds with a status 200", async() => {
+    expect(res.status).toBe(200)
+  });
+  it("has a message", async() => {
+    expect(res.body.message).toBe('welcome, marego')
+  });
+  it("has a token", async() => {
+    expect(res.body.token).toBeDefined();
+  });
+});
