0ef86818d813c7c44e1f04346c286f8301bc2649
Arison13 <arisonarias@gmail.com>
2021-12-17 12:10:17 -0500

    user-model and token builder done

---
+++ api/users/users-model.js
@@ -0,0 +1,21 @@
+const db = require("./../../data/dbConfig");
+
+function findUserbyId(id) {
+  return db("users").where("id", id).first();
+}
+
+function findBy(filter) {
+  return db("users").where(filter).first();
+}
+
+async function add(user) {
+  const newId = await db("users").insert(user);
+
+  return findUserbyId(newId);
+}
+
+module.exports = {
+  findUserbyId,
+  findBy,
+  add,
+};
